c5e41b1d6d2bfc47f47d647beaf1b1cd09fe14f1
Yosmarli Parica <101285117+Yosmarli@users.noreply.github.com>
2023-02-07 16:07:38 -0400

    Add files via upload

---
+++ Conjuntos.js
@@ -0,0 +1,8 @@
+const familia = new Set (["Yosmarli", "Jossie", "Jorge", "Carlos", "Javier"])
+console.log(familia)
+
+familia.add("Yosmarli")
+console.log(familia)
+
+familia.add("JavaScript")
+console.log(familia)
